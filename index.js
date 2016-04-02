ff88f537f7cffb095e41111516b09a5175b9a5d4
Gaurav S.J.B. Rana <bitcoinssg@gmail.com>
2016-04-02 16:27:52 +0545

    resolved compatibility with monero

--- index.js
+++ index.js
@@ -15,51 +15,20 @@ program
  .version('0.0.1')
  .option('-d, --destination ' + chalk.red('<required>'), 'The address where you want your funds sent after exchange.')
  .option('-r, --refund '+ chalk.red('<required>'), 'The refund address')
- .option('-x, --extra ' + chalk.yellow('<optional>') , 'The extra data required for special exchanges (e.g. Payment Id, incase of monero)')
+ .option('-x, --extra ' + chalk.yellow('<optional>') , 'The extra data required for special exchanges')
  .option('-p, --pair ' + chalk.red('<required>') ,'The pair in conversion (e.g. bitcoin to ether; btc_eth)')
  .parse(process.argv);
 
 console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:') + '	welcome to rapidshift version ' + program._version + ' - @bitcoinssg');
 gchecks.checkall(program)
 .then(accounting.populateBeforeExchange)
+.then(printExchangeInfoBeforeExchange)
+.then(prepareOptionsForShiftApi)
+.then(shiftwithpromise)
 .then(function(){
-	//console.log(accounting);
-	printExchangeInfo(accounting);
-})
-.then(function(){
-	var deposit_limit = 0;
-	shapeshift.marketInfo(program.pair, function (err, marketInfo) {
-	  if(err){
-	  	console.log('there was an error from marketinfo');
-	  	console.log(err);
-	  }
-	  else {
-	  		deposit_limit = marketInfo.limit;
-	  		// console.log('');
-	  		// console.log(marketInfo);
-	  		// console.log('');
-	  		var options = {
-	  			returnAddress: program.refund
-	  		}
-			//shapeshift.shift(program.destination, program.pair, options, function (err, returnData) {
-			mockfunctions.mockshapeshiftshift(program.destination, program.pair, options, function (err, returnData) {
-				  if(err){
-				  	console.log('there was an error during shift');
-				  	console.log(err);
-
-				  }
-
-				  var depositAddress = returnData.deposit
-				  //console.log(returnData);
-
-				  console.log("");
-				  console.log( "send upto " + chalk.green(deposit_limit) + " to " + chalk.green(depositAddress) );
-
-				  loopforcompletestatus(depositAddress);
-			});
-	  }
-	})
-
+	loopforcompletestatus({ no_deposits: 0, received: 0, complete: 0}, function(){
+		printExchangeAfterExchange();
+	});
 })
 .catch(function(err){
 	process.stdout.write("\n\n");
@@ -70,51 +39,131 @@ gchecks.checkall(program)
 
 
 
-function printExchangeInfo(accounting){
+
+
+
+
+
+
+
+function shiftwithpromise(options){
 	var deferred = Q.defer();
-	console.log('');
-	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + accounting.sourceCoin.fullname + ' price: ($'+ accounting.sourceCoin.priceFromExternal_USD + ')');
-	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + '	' + '|');
-	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + '	' + 'V');
-	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + 
-		accounting.destinationCoin.fullname + ' price: ($'+ accounting.destinationCoin.priceFromExternal_USD + ')	'  + 
-		' implied price: ' + '($' + accounting.destinationCoin.priceProposedDuringExchange_USD + ') = ' +
-		accounting.expectedVsActual.percentageOfExpected+ '%');
+	//mockfunctions.mockshapeshiftshift(program.destination, program.pair, options, function (err, returnData) {
+	shapeshift.shift(program.destination, program.pair, options, function (err, returnData) {
+		if(err){
+			console.log('there was an error during shift');
+			console.log(err);
+		}
+		else{
+		  console.log("");
+		  console.log("");
+		  console.log( chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') +
+		   		 "Send " + accounting.sourceCoin.fullname + ":	" + chalk.bgBlue(accounting.exchangeLimit.amount) + " (MAX)");
+		  if(accounting.sourceCoin.symbol == 'xmr')
+			{
+				// when monero is source handle payment id from shapeshift
+				console.log( chalk.dim('[' + moment().format('hh:mm:ss a') +']:	')  +"Address:	" + chalk.bgBlue(returnData.sAddress));
+				console.log( chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') +  "paymentID:	" + chalk.bgWhite(chalk.black(returnData.deposit)));
+			}
+		   else{
+		   		console.log( chalk.dim('[' + moment().format('hh:mm:ss a') +']:	')  +"Address:	" + chalk.bgBlue(returnData.deposit) );
+		   }
+		  console.log("");
+		  console.log("");
+		  //console.log(returnData);
+		  accounting.depositAddress = returnData.deposit;
+		  //loopforcompletestatus(returnData.deposit);
+		  deferred.resolve()
+		}
+	});
 	return deferred.promise;
 }
 
 
 
 
-function loopforcompletestatus(depositAddress, counts ){
+// extra preparation for multiple coin type handling
+function prepareOptionsForShiftApi(){  
+	var deferred = Q.defer();
+	var options = { returnAddress: program.refund };
+	if(accounting.exchangeLimit.amount > 0){
+		if(accounting.destinationCoin.symbol == 'xmr')
+		{
+			// treat monero is destination coin then prepare with payment id in options
+			// turns out we don't need to do anything from out side when we're receiving monero
+			// when we are sending monero a payment id is received and handled by the output
+		}
+		deferred.resolve(options);
+	}
+	else{
+		deferred.reject("Shapeshift limit is 0, try again in a few minutes.")
+	}
+
+	return deferred.promise;
+}
+
+function printExchangeAfterExchange(){
+	var deferred = Q.defer();
+	console.log('');
+	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + 
+		accounting.destinationCoin.fullname + ' price: ($'+ accounting.destinationCoin.priceFromExternal_USD + ')	'  + 
+		' actual price: ' + '($' + accounting.destinationCoin.priceCostAfterExchange_USD + ') = ' +
+		accounting.percentages.percentCostAfterExchange + '%' );
+	deferred.resolve();
+	return deferred.promise;
+}
+
+
+function printExchangeInfoBeforeExchange(){
+	var deferred = Q.defer();
+	console.log('');
+	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + accounting.sourceCoin.fullname + ' price: ($'+ accounting.sourceCoin.priceFromExternal_USD + ')');
+	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + '   |');
+	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + '   V');
+	console.log(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + 
+		accounting.destinationCoin.fullname + ' price: ($'+ accounting.destinationCoin.priceFromExternal_USD + ')	'  + 
+		' implied price: ' + '($' + accounting.destinationCoin.priceProposedDuringExchange_USD + ') = ' +
+		accounting.percentages.percentProposedDuringExchange + '%');
+	deferred.resolve();
+	return deferred.promise;
+}
+
+
+
+
+function loopforcompletestatus(counts,callback){
 	var shift_status = "no_deposits";
+	var depositAddress = accounting.depositAddress;
 	var counts = counts || { no_deposits: 0, received: 0, complete: 0};
 	setTimeout(function(){
-		//shapeshift.status(depositAddress, function (err, status, data) {
-		mockfunctions.mockshapeshiftstatus(depositAddress, function (err, status, data) {
+		shapeshift.status(depositAddress, function (err, status, data) {
+		//mockfunctions.mockshapeshiftstatus(depositAddress, function (err, status, data) {
 		   if(status == "no_deposits"){ 
-		   	(counts.no_deposits == 0) ? process.stdout.write(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	')+ chalk.red(":-\\  	waiting for deposit 	")) : process.stdout.write(chalk.red("."));
+		   	(counts.no_deposits == 0) ? process.stdout.write(chalk.dim('[' + moment().format('hh:mm:ss a') +']:	')+ "waiting for deposit 	") : process.stdout.write(chalk.red("."));
 		   	 counts.no_deposits++;
 		   }
 		   else if(status == "received"){
-		   	(counts.received == 0) ?process.stdout.write('\n' + chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + chalk.yellow(":-| 	"  + data.incomingCoin + " " + data.incomingType + " received 	")) : process.stdout.write(chalk.yellow("."));
+		   	(counts.received == 0) ?process.stdout.write('\n' + chalk.dim('[' + moment().format('hh:mm:ss a') +']:	')  + data.incomingCoin + " " + data.incomingType + " received 	") : process.stdout.write(chalk.yellow("."));
 		   	counts.received++;
 		   }
 		   else if(status == "complete"){
-		   	process.stdout.write('\n' + chalk.dim('[' + moment().format('hh:mm:ss a') +']:	') + chalk.green(":-) 	")); 
-		   	console.log(chalk.green(data.outgoingCoin) + " " + chalk.green(data.outgoingType + " sent 	✓"));
-		   	console.log("");
-		   	console.log("");
+		   	process.stdout.write('\n' + chalk.dim('[' + moment().format('hh:mm:ss a') +']:	')); 
+		   	console.log(data.outgoingCoin + " " + data.outgoingType + "	sent	" + chalk.bgGreen("Success"));
+		   	console.log('');
+		   	accounting.populateAfterExchange(data).then(function(returneddata){
+		   		callback();
+		   	})
+
 		   }
 		   else{
 		   	console.log('error');
 		   }
 		   shift_status = status;
 		   if( shift_status != "complete"){
-		   		loopforcompletestatus(depositAddress, counts);
+		   		loopforcompletestatus(counts,callback);
 		   }
 	  	})
-	},100)
+	},15000)
 }
 
 
--- tests/mockfunctions.js
+++ tests/mockfunctions.js
@@ -10,14 +10,15 @@ module.exports = {
 
 // this function mimicks the output of shapeshift.io function shapeshift.shift for testing
 function mockshapeshiftshift(destination, programpair, options, callback){
-	var mockreturneddata = { deposit: 'fake-deposit-address',
+  var mockreturneddata = { deposit: 'fake-deposit-address',
   depositType: programpair.split("_")[0],
   withdrawal: destination,
   withdrawalType: programpair.split("_")[1],
   public: null,
   apiPubKey: 'shapeshift',
   returnAddress: options.returnAddress,
-  returnAddressType: programpair.split("_")[0] };
+  returnAddressType: programpair.split("_")[0] 
+  };
   callback(null, mockreturneddata);
 }
 
@@ -31,19 +32,19 @@ function mockshapeshiftstatus(depositAddress, callback){
 		count++;
 		callback(null,'no_deposits',{ status: 'no_deposits', address: '0x5abd5a30cb5bf24759c19db1cdc1082dd9c5b618' });
 	}
-	else if( count > (threshold-1) && count < (threshold*6)){
+	else if( count > (threshold-1) && count < (threshold*3)){
 		count++;
-		var receiveddata =  { status: 'received',  address: '1H4vW9wcgr5i6LVsYMTuz449ci7ZWSyX6F', incomingCoin: 50 ,incomingType: 'ETH' };
+		var receiveddata =  { status: 'received',  address: '1H4vW9wcgr5i6LVsYMTuz449ci7ZWSyX6F', incomingCoin: 50.0 ,incomingType: 'ETH' };
 		callback(null,'received',receiveddata);
 	}
-	else if( count > ((threshold*6)-1)){
+	else if( count > ((threshold*3)-1)){
 		count = 0;
 		var completeddata = { status: 'complete', 
 		  address: '0x3d0386fae7d55841cd0613dfa03b8acdf73f4344',
 		  withdraw: '1Eqx6khcVVL2UGvWh5pb7yHC6tTLfcP3SU',
-		  incomingCoin: 0.006014991818181819,
+		  incomingCoin: 50.0,
 		  incomingType: 'ETH',
-		  outgoingCoin: '0.00017076',
+		  outgoingCoin: '1.345',
 		  outgoingType: 'BTC',
 		  transaction: '7be3b029d36eaf91ecf5d1474677136396bdb03e2f207f79714f457f5843520e' 
 		};
